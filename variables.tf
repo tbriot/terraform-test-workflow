28b9754cf7cbde9dc93b05650a1c8fbc7681f8fa
Thomas Briot <thomas.briot82@gmail.com>
2023-11-05 11:38:56 -0500

    docs: create config file for terraform-docs

--- variables.tf
+++ variables.tf
@@ -2,6 +2,29 @@ variable "s3_bucket_name" {
   type = string
 }
 
-variable "unused_var" {
+variable "a_var" {
+  type    = string
+  default = "myValue"
+}
+
+variable "b_var" {
+  type    = string
+  default = "myValue"
+}
+
+
+variable "var1_required" {
   type = string
+}
+
+variable "var2_required" {
+  type        = string
+  description = "this var is required and sensitive"
+  sensitive   = true
+}
+
+variable "var3_required" {
+  type        = string
+  description = "this var is required"
+  sensitive   = false
 }
