15b5001270aa808e51056d2b5c9c436ba071f050
Thomas Briot <thomas.briot82@gmail.com>
2024-09-29 20:45:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "test" {
       values = [
         "",
         "home/",
-        "home/test4",
+        "home/test5",
         "home/&{aws:username}/",
       ]
     }
