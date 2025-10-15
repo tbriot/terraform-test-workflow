97ff633fd14edf201a2f42dcb2a56a53f27c9bdb
Thomas Briot <thomas.briot82@gmail.com>
2025-10-15 18:57:57 +0200

    Change ingress port from 443 to 80

--- main.tf
+++ main.tf
@@ -65,8 +65,8 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = [data.aws_vpc.selected.cidr_block]
   }
